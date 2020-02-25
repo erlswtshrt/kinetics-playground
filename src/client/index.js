a784635a86d83a5846094d42c6964ccaffaf34df
Doug Molidor <dmolidor@salesforce.com>
2020-02-25 00:48:49 -0800

    kyt with SLDS

---
+++ src/client/index.js
@@ -0,0 +1,28 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Root from './Root';
+
+import styles from '../shared-styles/slds.scss';
+
+const root = document.querySelector('#root');
+
+const mount = RootComponent => {
+  render(
+    <AppContainer>
+      <RootComponent />
+    </AppContainer>,
+    root
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./Root', () => {
+    // eslint-disable-next-line global-require,import/newline-after-import
+    const RootComponent = require('./Root').default;
+    mount(RootComponent);
+  });
+}
+
+mount(Root);
