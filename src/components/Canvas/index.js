b5190bd02911521894bde2324e787fa4916da48f
John Earle <j.earle@salesforce.com>
2020-02-25 18:02:38 -0800

    menu animation

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -4,105 +4,106 @@ class Canvas extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      selectedComponent: 0,
+      selectedComponent: 'menuProps',
+      duration: 1,
       durationValues: [0.2, 0.3, 0.5, 1],
       easeValues: ["ease-in", "ease-out", "ease-in-out"],
-      componentProps: [{
+
+      menuValue: 40,
+      button1Value: 0,
+      button2Value: 0,
+      button3Value: 0,
+
+      menuProps: {
         property: "height",
         duration: 0.2,
-        value: 0,
+        initialValue: 40,
         finalValue: 300,
         ease: "ease-in"
       },
-      {
-        property: "height",
+      button1Props: {
+        property: "opacity",
         duration: 0.2,
-        value: 0,
         finalValue: 300,
         ease: "ease-in"
-      }]
+      },
+      button2Props: {
+        property: "opacity",
+        duration: 0.2,
+        finalValue: 300,
+        ease: "ease-in"
+      },
+      button3Props: {
+        property: "opacity",
+        duration: 0.2,
+        finalValue: 300,
+        ease: "ease-in"
+      },
     };
   }
 
   animate() {
     this.setState({
-      value: this.state.finalValue
+      menuValue: this.state.menuProps.finalValue,
+      button1Value: this.state.button1Props.finalValue,
+      button2Value: this.state.button2Props.finalValue,
+      button3Value: this.state.button3Props.finalValue,
     });
+
     setTimeout(() => {
-      this.reset();
+      this.setState({
+        menuValue: this.state.menuProps.initialValue,
+        button1Value: this.state.button1Props.initialValue,
+        button2Value: this.state.button2Props.initialValue,
+        button3Value: this.state.button3Props.initialValue,
+      });
     }, this.state.duration * 1000 + 2000);
   }
 
-  reset() {
-    this.setState({
-      value: 0
-    });
+  renderPropMenu() {
+    return <div><label>Ease curve</label>
+      <select
+        value={this.state.ease}
+        onChange={e =>
+          this.setState({
+            ease: e.target.value
+          })
+        }
+      >
+        {this.state.easeValues.map(v => (
+          <option value={v}>{v}</option>
+        ))}
+      </select>
+      <label>Duration</label>
+      <select
+        value={this.state.duration}
+        onChange={e =>
+          this.setState({
+            duration: e.target.value
+          })
+        }
+      >
+        {this.state.durationValues.map(v => (
+          <option value={v}>{v}</option>
+        ))}
+      </select></div>
   }
 
   render() {
-    const comp1Props = this.state.componentProps[0]
-    const comp2Props = this.state.componentProps[1]
+    const menuProps = this.state.menuProps
+    const menuStyle = {
+      width: 40,
+      background: "blue",
+      transition: this.state.menuValue === menuProps.finalValue ? `${menuProps.property} ${menuProps.duration}s ${menuProps.ease}` : null,
+      height: this.state.menuValue
+    }
+
     return (
       <div>
         <div style={{ width: 800, height: 600, background: "red" }}>
-          <h2>Component 1</h2>
-          <div
-            style={{
-              width: 40,
-              background: "blue",
-              transition: comp1Props.value === comp1Props.finalValue ? `${comp1Props.property} ${comp1Props.duration}s ${comp1Props.ease}` : null,
-              height: comp1Props.value
-            }}
-          />
-          <h2>Component 2</h2>
-          <div
-            style={{
-              width: 80,
-              background: "blue",
-              transition: comp2Props.value === comp2Props.finalValue ? `${comp2Props.property} ${comp2Props.duration}s ${comp2Props.ease}` : null,
-              height: comp2Props.value
-            }}
-          />
+          <div style={menuStyle}>Menu</div>
         </div>
-        <label>Component to animate</label>
-        <select
-          value={this.state.selectedComponent}
-          onChange={e =>
-            this.setState({
-              selectedComponent: e.target.value
-            })
-          }
-        >
-          {[0, 1].map(v => (
-            <option value={v}>{v}</option>
-          ))}
-        </select>
-        <label>Ease curve</label>
-        <select
-          value={this.state.ease}
-          onChange={e =>
-            this.setState({
-              ease: e.target.value
-            })
-          }
-        >
-          {this.state.easeValues.map(v => (
-            <option value={v}>{v}</option>
-          ))}
-        </select>
-        <label>Duration</label>
-        <select
-          value={this.state.duration}
-          onChange={e =>
-            this.setState({
-              duration: e.target.value
-            })
-          }
-        >
-          {this.state.durationValues.map(v => (
-            <option value={v}>{v}</option>
-          ))}
-        </select>
+        {this.renderPropMenu()}
         <button onClick={this.animate.bind(this)}>Animate</button>
       </div>
     );
